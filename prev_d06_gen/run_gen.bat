1d6d29a84afe526857d026ee684129d124681d2f
Alexandre Andrade <seu-email@gmail.com>
2026-06-13 16:05:17 -0300

    feat: PREV_D06 Politica Nacional de Saude e Legislacao do SUS - 48 flashcards + 100 questoes (quiz 001/002, via API; correcao 10 questoes HARD quiz_002 com 5 alternativas reduzidas para 4)

---
+++ prev_d06_gen/run_gen.bat
@@ -0,0 +1,4 @@
+@echo off
+set ANTHROPIC_API_KEY=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
+cd /d C:\ARAGORN\aragorn_quiz\prev_d06_gen
+python gerar_dominio.py PREV_D06 "Politica Nacional de Saude e Legislacao do SUS" "Medicina Preventiva e Social"
